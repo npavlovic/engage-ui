f831d5f8eafd21d98c9a3da50ae1d74d29feb511
Jonathan Kim <kimhjona@gmail.com>
2017-08-31 16:40:15 -0700

    added accessibility to docs

--- docs/src/reducers/InitialState.ts
+++ docs/src/reducers/InitialState.ts
@@ -2,11 +2,13 @@ import { IDocumentAppState } from '../Types';
 
 import ChoiceListState from './componentState/ChoiceListState';
 import PanelState from './componentState/PanelState';
+import AccessibilityState from './componentState/AccessibilityState';
 
 const intialState : IDocumentAppState = {
   components: [
     ChoiceListState,
     PanelState,
+    AccessibilityState,
   ],
 };
 
---
+++ docs/src/reducers/componentState/AccessibilityState.tsx
@@ -0,0 +1,9 @@
+import { IDocument } from '../../Types';
+
+const AccessibilityState: IDocument = {
+  id: 'accessibility',
+  heading : 'Accessibility Component',
+  subheading : 'Docs!!',
+};
+
+export default AccessibilityState;
