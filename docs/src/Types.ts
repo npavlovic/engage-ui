f99edb1092f65b1a8c156a2410469e1c33c518ce
Jonathan Kim <kimhjona@gmail.com>
2017-09-01 15:31:12 -0700

    documentation setup stuff

--- docs/src/Types.ts
+++ docs/src/Types.ts
@@ -13,6 +13,7 @@ export interface IDocument {
   exampleComponent?: any;
   exampleCodeExtra?: string;
   exampleComponentExtra?: any;
+  text?: HTMLInputElement;
 }
 
 export interface IProperty {
--- docs/src/reducers/componentState/AccessibilityState.tsx
+++ docs/src/reducers/componentState/AccessibilityState.tsx
@@ -2,8 +2,9 @@ import { IDocument } from '../../Types';
 
 const AccessibilityState: IDocument = {
   id: 'accessibility',
-  heading : 'Accessibility Component',
+  heading : 'Accessibility',
   subheading : 'Docs!!',
+  // text: <html></html>,
 };
 
 export default AccessibilityState;
